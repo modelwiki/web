8303894af0d2e27770c094135434b155426f0095
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2021-02-04 19:58:06 +0100

    Workaround for WSL

--- serve.sh
+++ serve.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-set -euo pipefail
+# When this is running, sign in at http://localhost:5000/index.html
+
+# Joakim gets an error for: o pipefail
+set -eu
 
 cd app && npm run-script build
 
 firebase serve
+
