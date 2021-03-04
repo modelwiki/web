eb1cf027b57059937df9d81fb87d7d075922a1cb
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2021-03-04 21:25:46 +0100

    Slide to change input value

--- app/src/components/InputVariable.tsx
+++ app/src/components/InputVariable.tsx
@@ -20,6 +20,7 @@ function InputVariable(props: {value: number, text: string, descriptor: Descript
     }
   }
 
+  // TODO: Use memo for onMouseMove to make removeEventListener work
   React.useEffect(() => {
     if(sliding) {
         document.addEventListener("mousemove", onMouseMove);
